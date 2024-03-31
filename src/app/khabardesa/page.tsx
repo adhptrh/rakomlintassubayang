6aa31595bcee33606e6f9d78b1c7d611d1347287
adhptrh <70842975+adhptrh@users.noreply.github.com>
2024-03-31 12:22:33 +0700

    khabardesa page

---
+++ src/app/khabardesa/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Navbar from "../../components/Navbar";
+import BigNews from "../../components/BigNews";
+import NewsList from "@/components/NewsList";
+
+export default function KhabarDesa() {
+  return (
+    <main className="flex flex-col min-h-screen items-center">
+      <Navbar />
+      <div className="w-full h-[80px]"></div>
+      
+
+      
+      <div className="w-full bg-black flex justify-center pt-5">
+        <div className="container px-5 flex w-full flex-col text-white">
+          <p className="font-bold mb-8">Footer</p>
+        </div>
+      </div>
+
+    </main>
+  );
+}
