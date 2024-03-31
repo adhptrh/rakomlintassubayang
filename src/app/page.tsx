52c0a9b9d221b583aba3017ca5368637083c1f24
adhptrh <70842975+adhptrh@users.noreply.github.com>
2024-03-31 12:21:47 +0700

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,13 @@ export default function Home() {
       <NewsList />
       <div className="mb-10"></div>
 
+      
+      <div className="w-full bg-black flex justify-center pt-5">
+        <div className="container px-5 flex w-full flex-col text-white">
+          <p className="font-bold mb-8">Footer</p>
+        </div>
+      </div>
+
     </main>
   );
 }
