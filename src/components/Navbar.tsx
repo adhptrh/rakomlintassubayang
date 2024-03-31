f2b57a5ea712cd8df3e7d4e11820079554302a7a
adhptrh <70842975+adhptrh@users.noreply.github.com>
2024-03-31 18:06:39 +0700

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,8 +12,8 @@ function Navbar () {
                 <div className="w-full mt-5 h-10 flex">
                     <img src="./logo.png" width={100}></img>
                     <div className="w-[1px] h-full ml-16 bg-black"></div>
-                    <Link href="/"           className={"md:flex hidden h-full ml-10 flex items-center text-sm " + (pathname == "/" ? "font-bold text-custom-blue-sky border-custom-blue-sky border-b-2":"text-slate-400")}>BERANDA</Link>
-                    <Link href="/khabardesa" className={"md:flex hidden h-full ml-10 flex items-center text-sm " + (pathname == "/khabardesa" ? "font-bold text-custom-blue-sky border-custom-blue-sky border-b-2":"text-slate-400")}>KHABAR DESA</Link>
+                    <Link href="/"           className={"md:flex hidden h-full ml-10 flex items-center transition-all text-sm " + (pathname == "/" ? "font-bold text-custom-blue-sky border-custom-blue-sky border-b-2":"text-slate-400")}>BERANDA</Link>
+                    <Link href="/khabardesa" className={"md:flex hidden h-full ml-10 flex items-center transition-all text-sm " + (pathname == "/khabardesa" ? "font-bold text-custom-blue-sky border-custom-blue-sky border-b-2":"text-slate-400")}>KHABAR DESA</Link>
                     <div className="flex-1 justify-end flex">
                         <Link href="/live" className="h-full font-bold text-red-500 border-2 border-red-500 flex items-center px-8 text-sm">LIVE RADIO</Link>
                     </div>
