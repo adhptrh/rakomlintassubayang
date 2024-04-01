a9a17d1e8821244024e1ab9a6915fc1a30611253
adhptrh <70842975+adhptrh@users.noreply.github.com>
2024-04-01 09:52:01 +0700

    animation

--- src/components/BigNews.tsx
+++ src/components/BigNews.tsx
@@ -5,7 +5,7 @@ function BigNews() {
         <p className="font-bold mb-5">Berita</p>
         <div className="w-full gap-x-6 flex mb-3 h-[300px] md:h-[450px] xl:h-[600px]">
           <div className="flex-1 h-full">
-            <div className="relative w-full bg-black h-full rounded-lg bg-center bg-cover shadow-2xl flex flex-col justify-end" style={{backgroundImage: "url(./thumb.jpeg)"}}>
+            <div className="relative w-full bg-black h-full cursor-pointer hover:mt-[-10px] transition-all rounded-lg bg-center bg-cover shadow-2xl flex flex-col justify-end" style={{backgroundImage: "url(./thumb.jpeg)"}}>
               <div className="h-32 z-10 rounded-lg absolute w-full bg-black opacity-70 border-t-2 border-custom-blue-sky text-white font-bold text-2xl">
               </div>  
               <div className="h-32 z-20 p-5 border-t-2 border-custom-blue-sky text-white font-bold text-xl md:text-2xl">
@@ -14,19 +14,19 @@ function BigNews() {
             </div>
           </div>
           <div className="flex-none flex-col gap-y-6 hidden w-0 lg:flex lg:w-72 xl:w-96 h-full">
-            <div className="flex-1 flex flex-col relative flex justify-end bg-center bg-cover rounded-lg shadow-md" style={{backgroundImage: "url(./thumb.jpeg)"}}>
+            <div className="flex-1 flex flex-col relative flex justify-end bg-center bg-cover rounded-lg shadow-md cursor-pointer" style={{backgroundImage: "url(./thumb.jpeg)"}}>
               <div className="absolute z-10 h-16 z-10 p-2 w-full border-t-2 bg-black opacity-70 border-custom-blue-sky text-white font-bold text-xl md:text-xs rounded-lg"></div>  
               <div className="h-16 z-20 p-2 border-t-2 border-custom-blue-sky w-full text-white font-bold lg:text-xs xl:text-sm rounded-lg">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et sapien orci.</p>
               </div>  
             </div>
-            <div className="flex-1 flex flex-col relative flex justify-end bg-center bg-cover rounded-lg shadow-md" style={{backgroundImage: "url(./thumb.jpeg)"}}>
+            <div className="flex-1 flex flex-col relative flex justify-end bg-center bg-cover rounded-lg shadow-md cursor-pointer" style={{backgroundImage: "url(./thumb.jpeg)"}}>
               <div className="absolute z-10 h-16 z-10 p-2 w-full border-t-2 bg-black opacity-70 border-custom-blue-sky text-white font-bold text-xl md:text-xs rounded-lg"></div>  
               <div className="h-16 z-20 p-2 border-t-2 border-custom-blue-sky w-full text-white font-bold lg:text-xs xl:text-sm rounded-lg">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et sapien orci.</p>
               </div>  
             </div>
-            <div className="flex-1 flex flex-col relative flex justify-end bg-center bg-cover rounded-lg shadow-md" style={{backgroundImage: "url(./thumb.jpeg)"}}>
+            <div className="flex-1 flex flex-col relative flex justify-end bg-center bg-cover rounded-lg shadow-md cursor-pointer" style={{backgroundImage: "url(./thumb.jpeg)"}}>
               <div className="absolute z-10 h-16 z-10 p-2 w-full border-t-2 bg-black opacity-70 border-custom-blue-sky text-white font-bold text-xl md:text-xs rounded-lg"></div>  
               <div className="h-16 z-20 p-2 border-t-2 border-custom-blue-sky w-full.
               3 text-white font-bold lg:text-xs xl:text-sm rounded-lg">
