854c0b0c4e9611da71dba6945cdc163148ff5dcf
adhptrh <70842975+adhptrh@users.noreply.github.com>
2024-05-05 05:26:02 +0700

    commit

--- src/components/BigNews.tsx
+++ src/components/BigNews.tsx
@@ -24,7 +24,7 @@ function BigNews() {
               <div className="absolute z-10 h-16 z-10 p-2 w-full border-t-2 bg-black opacity-70 border-custom-blue-sky text-white font-bold text-xl md:text-xs rounded-2xl"></div>  
               <div className="h-16 z-20 p-2 border-t-2 border-custom-blue-sky w-full text-white font-bold lg:text-xs xl:text-sm rounded-2xl">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et sapien orci.</p>
-              </div>  
+              </div>
             </div>
             <div className="flex-1 flex flex-col relative flex justify-end bg-center bg-cover rounded-2xl shadow-md" style={{backgroundImage: "url(./thumb.jpeg)"}}>
               <div className="absolute z-10 h-16 z-10 p-2 w-full border-t-2 bg-black opacity-70 border-custom-blue-sky text-white font-bold text-xl md:text-xs rounded-2xl"></div>  
